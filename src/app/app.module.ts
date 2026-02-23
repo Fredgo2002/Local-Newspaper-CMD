84edffa68a3a125dfb8abf1cdf7f314e90bd40f4
Fredgo2002 <fagomez@espol.edu.ec>
2026-02-22 19:10:05 -0500

    style: first mocked data and visual row options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatOptionModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticlesHomeComponent } from './components/articles-home/articles-home.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
@@ -23,11 +30,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
+    FormsModule,
     MatSelectModule,
     MatInputModule,
     MatOptionModule,
     MatIconModule,
     MatButtonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/articles-home/articles-home.component.ts
+++ src/app/components/articles-home/articles-home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-articles-home',
   templateUrl: './articles-home.component.html',
-  styleUrls: ['./articles-home.component.css']
+  styleUrls: ['./articles-home.component.css'],
 })
-export class ArticlesHomeComponent {
+export class ArticlesHomeComponent implements OnInit {
+  isOpen = false;
+  articles: any[] = [];
+
+  constructor(
+    private articlesService: ArticlesService
+  ) { }
+
+  ngOnInit(): void {
+    this.articlesService.getArticles().subscribe((articles) => {
+      this.articles = articles;
+      console.log(this.articles);
+    });
+  }
 
 }
